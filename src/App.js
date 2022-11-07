2f434a4c8f9a9f28f7c406005a4dc404d2d3ccf2
mirajulalam <alifmirajulalam@gmail.com>
2022-11-07 11:48:54 -0800

    navbar components added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='grid place-items-center bg-blue-100 h-screen px-6 font-sans'>
+    <Navbar />
+  </div>
   );
 }
 
