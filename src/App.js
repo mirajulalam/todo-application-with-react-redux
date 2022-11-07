d5aaec23a324b250bb26b9a81e96b890195bab98
mirajulalam <alifmirajulalam@gmail.com>
2022-11-07 12:02:33 -0800

    header components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Header from './components/Header';
 import Navbar from './components/Navbar';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className='grid place-items-center bg-blue-100 h-screen px-6 font-sans'>
     <Navbar />
+    <div className='w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white'>
+					<Header />
+
+					<hr className='mt-4' />
+					<TodoList />
+					<hr className='mt-4' />
+
+					
+				</div>
   </div>
   );
 }
